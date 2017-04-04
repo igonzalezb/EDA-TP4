39875bea86a58cfce395fb816727f8290da3ba8d
Ian Diaz <idiaz@itba.edu.ar>
2017-04-03 22:59:57 -0300

    graphics dispatcher

--- EDA-WORMS/Graphics.cpp
+++ EDA-WORMS/Graphics.cpp
@@ -14,19 +14,12 @@ Graphics::Graphics()
 {
 	if(allegro_setup() == -1)
 		fprintf(stderr, "ALLEGRO ERROR\n");
-
-	al_set_window_title(display, "SEGA GENESIS-WOMRS");
-
 	intro(display,SCREEN_W,SCREEN_H);
 	for (uint i = 0; i < 6; i++) { key_pressed[i] = false; } //Estado de teclas, true cuando esta apretada
-	
-	if (loadImages() == -1)
-		fprintf(stderr, "ALLEGRO ERROR\n");
-
 	redraw = false;
 	do_exit = false;
-
-
+	if (loadImages() == -1)
+		fprintf(stderr, "ALLEGRO ERROR\n");
 }
 
 int Graphics::GraphicsMain()
@@ -34,132 +27,31 @@ int Graphics::GraphicsMain()
 	al_register_event_source(event_queue, al_get_display_event_source(display));
 	al_register_event_source(event_queue, al_get_timer_event_source(timer));
 	al_register_event_source(event_queue, al_get_keyboard_event_source()); //REGISTRAMOS EL TECLADO
-
 	al_clear_to_color(al_map_rgb(218, 227, 125));
 	al_draw_bitmap(Scenario, 0.0, 0.0, 0);
 	al_flip_display();
 	al_start_timer(timer);
-	
+	miliseconds = false;
 
 	while (!do_exit)
 	{
-
 		ALLEGRO_EVENT ev;
 
-		if (al_get_next_event(event_queue, &ev))
+		al_wait_for_event(event_queue, &ev);
+		dispatcher(ev);
+
+		if (redraw) 
 		{
-			if (ev.type == ALLEGRO_EVENT_TIMER)
-
-			{
-				if (key_pressed[KEY_UP] && worm2.whatAmIDoing() == STILL)
-					worm2.startJumping();
-
-				if (key_pressed[KEY_LEFT] && worm2.whatAmIDoing() == STILL)
-				{
-					worm2.lookRight(false);
-					worm2.startMoving();
-				}
-
-				if (key_pressed[KEY_RIGHT] && worm2.whatAmIDoing() == STILL)
-				{
-					worm2.lookRight(true);
-					worm2.startMoving();
-				}
-
-				if (key_pressed[KEY_W] && worm1.whatAmIDoing() == STILL)
-					worm1.startJumping();
-
-				if (key_pressed[KEY_A] && worm1.whatAmIDoing() == STILL)
-				{
-					worm1.lookRight(false);
-					worm1.startMoving();
-				}
-
-				if (key_pressed[KEY_D] && worm1.whatAmIDoing() == STILL)
-				{
-					worm1.lookRight(true);
-					worm1.startMoving();
-				}
-
-				redraw = true;
-			}
-
-
-			else if (ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
-				do_exit = true;
-
-			else if (ev.type == ALLEGRO_EVENT_KEY_DOWN)
-			{
-				switch (ev.keyboard.keycode) {
-				case ALLEGRO_KEY_UP:
-					if (worm2.whatAmIDoing() == STILL)
-						key_pressed[KEY_UP] = true;
-					break;
-				case ALLEGRO_KEY_LEFT:
-					if (worm2.whatAmIDoing() == STILL || worm2.whatAmIDoing() == FINISHING_WALKING)
-						key_pressed[KEY_LEFT] = true;
-					break;
-				case ALLEGRO_KEY_RIGHT:
-					if (worm2.whatAmIDoing() == STILL || worm2.whatAmIDoing() == FINISHING_WALKING)
-						key_pressed[KEY_RIGHT] = true;
-					break;
-				case ALLEGRO_KEY_W:
-					if (worm1.whatAmIDoing() == STILL)
-						key_pressed[KEY_W] = true;
-					break;
-				case ALLEGRO_KEY_A:
-						if (worm1.whatAmIDoing() == STILL || worm1.whatAmIDoing() == FINISHING_WALKING)
-						key_pressed[KEY_A] = true;
-					break;
-				case ALLEGRO_KEY_D:
-					if (worm1.whatAmIDoing() == STILL || worm1.whatAmIDoing() == FINISHING_WALKING)
-						key_pressed[KEY_D] = true;
-					break;
-				}
-			}
-
-			else if (ev.type == ALLEGRO_EVENT_KEY_UP)
-			{
-				switch (ev.keyboard.keycode) {
-				case ALLEGRO_KEY_UP:
-					key_pressed[KEY_UP] = false;
-					break;
-
-				case ALLEGRO_KEY_LEFT:
-					key_pressed[KEY_LEFT] = false;
-					break;
-
-				case ALLEGRO_KEY_RIGHT:
-					key_pressed[KEY_RIGHT] = false;
-					break;
-				case ALLEGRO_KEY_W:
-					key_pressed[KEY_W] = false;
-					break;
-				case ALLEGRO_KEY_A:
-					key_pressed[KEY_A] = false;
-					break;
-				case ALLEGRO_KEY_D:
-					key_pressed[KEY_D] = false;
-					break;
-
-				case ALLEGRO_KEY_ESCAPE:
-					do_exit = true;
-					break;
-				}
-			}
-
-			if (redraw) {
-				worm1.continueAction();
-				worm2.continueAction();
-				redraw = false;
-				al_clear_to_color(al_map_rgb(218, 227, 125));
-				al_draw_bitmap(Scenario, 0.0, 0.0, 0);
-				printWorm(worm1);
-				printWorm(worm2);
-				al_flip_display();
-				
-			}
+			worm1.continueAction();
+			worm2.continueAction();
+			redraw = false;
+			al_clear_to_color(al_map_rgb(218, 227, 125));
+			al_draw_bitmap(Scenario, 0.0, 0.0, 0);
+			al_draw_scaled_bitmap(wWalkF4, 0.0, 0.0, al_get_bitmap_width(wWalkF4), al_get_bitmap_height(wWalkF4), worm1.getX(), worm1.getY(), CUADRADITO_SIZE, CUADRADITO_SIZE, worm1._lookingRight());
+			al_draw_scaled_bitmap(wWalkF4, 0.0, 0.0, al_get_bitmap_width(wWalkF4), al_get_bitmap_height(wWalkF4), worm2.getX(), worm2.getY(), CUADRADITO_SIZE, CUADRADITO_SIZE, worm2._lookingRight());
+			al_flip_display();
 		}
+		
 
 		
 	}
@@ -170,71 +62,10 @@ int Graphics::GraphicsMain()
 	return 0;
 }
 
-void Graphics::printWorm(Worm worm)
-{
-	switch (worm.getCurrentFrame())
-	{
-	case 0: case 1: case 2: case 3: case 4:
-		al_draw_scaled_bitmap(wWalkF4, 0.0, 0.0, al_get_bitmap_width(wWalkF4), al_get_bitmap_height(wWalkF4), worm.getX(), worm.getY(), CUADRADITO_SIZE, CUADRADITO_SIZE, worm._lookingRight());
-		break;
-
-	case 5:
-		al_draw_scaled_bitmap(wWalkF4, 0.0, 0.0, al_get_bitmap_width(wWalkF4), al_get_bitmap_height(wWalkF4), worm.getX(), worm.getY(), CUADRADITO_SIZE, CUADRADITO_SIZE, worm._lookingRight());
-		break;
-	case 6: 
-		al_draw_scaled_bitmap(wWalkF5, 0.0, 0.0, al_get_bitmap_width(wWalkF5), al_get_bitmap_height(wWalkF5), worm.getX(), worm.getY(), CUADRADITO_SIZE, CUADRADITO_SIZE, worm._lookingRight());
-		break;
-	case 7: 
-		al_draw_scaled_bitmap(wWalkF6, 0.0, 0.0, al_get_bitmap_width(wWalkF6), al_get_bitmap_height(wWalkF6), worm.getX(), worm.getY(), CUADRADITO_SIZE, CUADRADITO_SIZE, worm._lookingRight());
-		break;
-	case 8: case 22: case 36:
-		al_draw_scaled_bitmap(wWalkF4, 0.0, 0.0, al_get_bitmap_width(wWalkF4), al_get_bitmap_height(wWalkF4), worm.getX(), worm.getY(), CUADRADITO_SIZE, CUADRADITO_SIZE, worm._lookingRight());
-		break;
-	case 9: case 23: case 37:
-		al_draw_scaled_bitmap(wWalkF5, 0.0, 0.0, al_get_bitmap_width(wWalkF5), al_get_bitmap_height(wWalkF5), worm.getX(), worm.getY(), CUADRADITO_SIZE, CUADRADITO_SIZE, worm._lookingRight());
-		break;
-	case 10: case 24: case 38:
-		al_draw_scaled_bitmap(wWalkF6, 0.0, 0.0, al_get_bitmap_width(wWalkF6), al_get_bitmap_height(wWalkF6), worm.getX(), worm.getY(), CUADRADITO_SIZE, CUADRADITO_SIZE, worm._lookingRight());
-		break;
-	case 11: case 25: case 39:
-		al_draw_scaled_bitmap(wWalkF7, 0.0, 0.0, al_get_bitmap_width(wWalkF6), al_get_bitmap_height(wWalkF6), worm.getX(), worm.getY(), CUADRADITO_SIZE, CUADRADITO_SIZE, worm._lookingRight());
-		break;
-	case 12: case 26: case 40:
-		al_draw_scaled_bitmap(wWalkF8, 0.0, 0.0, al_get_bitmap_width(wWalkF6), al_get_bitmap_height(wWalkF6), worm.getX(), worm.getY(), CUADRADITO_SIZE, CUADRADITO_SIZE, worm._lookingRight());
-		break;
-	case 13: case 27: case 41:
-		al_draw_scaled_bitmap(wWalkF9, 0.0, 0.0, al_get_bitmap_width(wWalkF6), al_get_bitmap_height(wWalkF6), worm.getX(), worm.getY(), CUADRADITO_SIZE, CUADRADITO_SIZE, worm._lookingRight());
-		break;
-	case 14: case 28: case 42:
-		al_draw_scaled_bitmap(wWalkF10, 0.0, 0.0, al_get_bitmap_width(wWalkF6), al_get_bitmap_height(wWalkF6), worm.getX(), worm.getY(), CUADRADITO_SIZE, CUADRADITO_SIZE, worm._lookingRight());
-		break;
-	case 15: case 29: case 43:
-		al_draw_scaled_bitmap(wWalkF11, 0.0, 0.0, al_get_bitmap_width(wWalkF6), al_get_bitmap_height(wWalkF6), worm.getX(), worm.getY(), CUADRADITO_SIZE, CUADRADITO_SIZE, worm._lookingRight());
-		break;
-	case 16: case 30: case 44:
-		al_draw_scaled_bitmap(wWalkF11, 0.0, 0.0, al_get_bitmap_width(wWalkF6), al_get_bitmap_height(wWalkF6), worm.getX(), worm.getY(), CUADRADITO_SIZE, CUADRADITO_SIZE, worm._lookingRight());
-		break;
-	case 17: case 31: case 45:
-		al_draw_scaled_bitmap(wWalkF12, 0.0, 0.0, al_get_bitmap_width(wWalkF6), al_get_bitmap_height(wWalkF6), worm.getX(), worm.getY(), CUADRADITO_SIZE, CUADRADITO_SIZE, worm._lookingRight());
-		break;
-	case 18: case 32: case 46:
-		al_draw_scaled_bitmap(wWalkF13, 0.0, 0.0, al_get_bitmap_width(wWalkF6), al_get_bitmap_height(wWalkF6), worm.getX(), worm.getY(), CUADRADITO_SIZE, CUADRADITO_SIZE, worm._lookingRight());
-		break;
-	case 19: case 33: case 47:
-		al_draw_scaled_bitmap(wWalkF14, 0.0, 0.0, al_get_bitmap_width(wWalkF6), al_get_bitmap_height(wWalkF6), worm.getX(), worm.getY(), CUADRADITO_SIZE, CUADRADITO_SIZE, worm._lookingRight());
-		break;
-	case 20: case 34: case 48:
-		al_draw_scaled_bitmap(wWalkF15, 0.0, 0.0, al_get_bitmap_width(wWalkF6), al_get_bitmap_height(wWalkF6), worm.getX(), worm.getY(), CUADRADITO_SIZE, CUADRADITO_SIZE, worm._lookingRight());
-		break;
-	case 21: case 35: case 49:
-		al_draw_scaled_bitmap(wWalkF4, 0.0, 0.0, al_get_bitmap_width(wWalkF4), al_get_bitmap_height(wWalkF4), worm.getX(), worm.getY(), CUADRADITO_SIZE, CUADRADITO_SIZE, worm._lookingRight());
-		break;
-	default:
-		al_draw_scaled_bitmap(wWalkF4, 0.0, 0.0, al_get_bitmap_width(wWalkF4), al_get_bitmap_height(wWalkF4), worm.getX(), worm.getY(), CUADRADITO_SIZE, CUADRADITO_SIZE, worm._lookingRight());
-		break;
-	}
-
-}
+//void printWorm(uint32_t frameCount, Worm worm)
+//{
+//
+//}
 
 int Graphics::allegro_setup()
 {
@@ -253,43 +84,6 @@ int Graphics::allegro_setup()
 		al_uninstall_system();
 		return -1;
 	}
-
-
-	if (!al_install_audio())
-	{
-		fprintf(stderr, "Unable to start audio addon \n");
-		al_uninstall_system();
-		return -1;
-	}
-	if (!al_install_keyboard())
-	{
-		fprintf(stderr, "Unable to start keyboard addon \n");
-		al_uninstall_system();
-		return -1;
-	}
-	if (!al_install_mouse())
-	{
-		fprintf(stderr, "Unable to start mouse addon \n");
-		al_uninstall_system();
-		return -1;
-	}
-	if (!al_init_acodec_addon())
-	{
-		fprintf(stderr, "Unable to start audio codec addon \n");
-		al_uninstall_system();
-		return -1;
-	}
-	if (!al_reserve_samples(1))
-	{
-		fprintf(stderr, "Unable to reserve samples \n");
-		al_uninstall_system();
-		return -1;
-	}
-	
-
-	al_init_font_addon(); // initialize the font addon
-	al_init_ttf_addon();	// initialize the ttf (True Type Font) addon
-
 
 	timer = al_create_timer(1.0 / FPS);
 	if (!timer) {
@@ -312,9 +106,6 @@ int Graphics::allegro_setup()
 		al_destroy_timer(timer);
 		return -1;
 	}
-
-	
-
 
 	return 0;
 }
@@ -498,4 +289,130 @@ int Graphics::loadImages()
 	
 
 	return 0;
+}
+
+void Graphics::dispatcher(ALLEGRO_EVENT ev)
+{
+	if (ev.type == ALLEGRO_EVENT_TIMER)
+	{
+		if (key_pressed[KEY_UP] && worm2.whatAmIDoing() == STILL)
+		{
+			worm2.startJumping();
+			key_pressed[KEY_UP]=false;
+		}
+		else if (key_pressed[KEY_LEFT] && (worm2.whatAmIDoing() == STILL|| worm2.whatAmIDoing()== FINISHING_WALKING))
+		{
+			if(miliseconds == true)
+			{
+				worm2.lookRight(false);
+				worm2.startMoving();
+				key_pressed[KEY_LEFT]=false;
+			}
+			else
+				miliseconds = true;
+		}
+
+		else if (key_pressed[KEY_RIGHT] && (worm2.whatAmIDoing() == STILL|| worm2.whatAmIDoing()== FINISHING_WALKING))
+		{
+			if(miliseconds==true)
+			{
+				worm2.lookRight(true);
+				worm2.startMoving();
+				key_pressed[KEY_RIGHT]=false;
+			}
+			else
+				miliseconds=true;
+		}
+		else if (key_pressed[KEY_W] && worm1.whatAmIDoing() == STILL)
+		{
+			worm1.startJumping();
+			key_pressed[KEY_W]=false;
+		}
+		else if (key_pressed[KEY_A] && (worm1.whatAmIDoing() == STILL || worm1.whatAmIDoing()== FINISHING_WALKING))
+		{
+			if(miliseconds==true)
+			{
+				worm1.lookRight(false);
+				worm1.startMoving();
+				key_pressed[KEY_A]=false;
+			}
+			else
+				miliseconds=true;
+		}
+
+		else if (key_pressed[KEY_D] && (worm1.whatAmIDoing() == STILL|| worm1.whatAmIDoing()== FINISHING_WALKING))
+		{
+			if(miliseconds==true)
+			{
+				worm1.lookRight(true);
+				worm1.startMoving();
+				key_pressed[KEY_D]=false;
+			}
+			else
+				miliseconds=true;
+		}
+		redraw = true;
+	}
+	else if (ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
+		do_exit = true;
+	else if (ev.type == ALLEGRO_EVENT_KEY_DOWN)
+	{
+		switch (ev.keyboard.keycode) {
+		case ALLEGRO_KEY_UP:
+			if (worm2.whatAmIDoing() == STILL)
+				key_pressed[KEY_UP] = true;
+			break;
+		case ALLEGRO_KEY_LEFT:
+			if (worm2.whatAmIDoing() == STILL || worm2.whatAmIDoing() == FINISHING_WALKING)
+				key_pressed[KEY_LEFT] = true;
+			break;
+		case ALLEGRO_KEY_RIGHT:
+			if (worm2.whatAmIDoing() == STILL || worm2.whatAmIDoing() == FINISHING_WALKING)
+				key_pressed[KEY_RIGHT] = true;
+			break;
+		case ALLEGRO_KEY_W:
+			if (worm1.whatAmIDoing() == STILL)
+				key_pressed[KEY_W] = true;
+			break;
+		case ALLEGRO_KEY_A:
+			if (worm1.whatAmIDoing() == STILL || worm1.whatAmIDoing() == FINISHING_WALKING)
+				key_pressed[KEY_A] = true;
+			break;
+		case ALLEGRO_KEY_D:
+			if (worm1.whatAmIDoing() == STILL || worm1.whatAmIDoing() == FINISHING_WALKING)
+				key_pressed[KEY_D] = true;
+			break;
+		}
+	}
+	else if (ev.type == ALLEGRO_EVENT_KEY_UP)
+	{
+		switch (ev.keyboard.keycode)
+		{
+			case ALLEGRO_KEY_UP:
+				key_pressed[KEY_UP] = false;
+				break;
+			case ALLEGRO_KEY_LEFT:
+				key_pressed[KEY_LEFT] = false;
+				miliseconds=false;
+				break;
+			case ALLEGRO_KEY_RIGHT:
+				key_pressed[KEY_RIGHT] = false;
+				miliseconds=false;
+				break;
+			case ALLEGRO_KEY_W:
+				key_pressed[KEY_W] = false;
+				break;
+			case ALLEGRO_KEY_A:
+				key_pressed[KEY_A] = false;
+				miliseconds=false;
+				break;
+			case ALLEGRO_KEY_D:
+				key_pressed[KEY_D] = false;
+				miliseconds=false;
+				break;
+			case ALLEGRO_KEY_ESCAPE:
+				do_exit = true;
+				break;
+		}
+	}
 }
